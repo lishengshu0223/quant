d9547547cf0e2c27e1db53d54ff37f44e11611f7
lishengshu0223 <lishengshu@126.com>
2023-05-23 09:22:49 +0800

    update my_model

--- src/model/my_model.py
+++ src/model/my_model.py
@@ -1,29 +1,16 @@
+import os
 from typing import Union
 
 import pandas as pd
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
+from sklearn.metrics import auc, roc_curve
 
 from src.dataset.dataset import SectionalDataset
-from src.model.base_function import train_loop, validation_loop
 
 
-def valid_loop(X_valid, y_valid):
-    """
-    所有验证集结果展示
-    :param eval_set:
-    :return:  验证集的评价指标，例：auc: xxx
-    """
-    auc = 0.60  # 举个例子，就是获取一个评判标准的数值
-    msg = "auc=xxx, loss=xxx, accuracy=xxx"
-    return msg, auc
-
-
-def predict(X_test):
-    return
-
-
-class BasicModel:
+class MyModel:
     def __init__(
         self,
         batch_size=1024,
@@ -39,6 +26,43 @@ class BasicModel:
         self.optimizer = optimizer
         self.loss_fn = loss_fn
         self.network = network(**network_kwargs)  # 需要用到的神经网络
+
+    def _trainloop(self, train_dataloader):
+        mean_loss = []
+        for x, y in train_dataloader:
+            pred = self.network(x).flatten()
+            loss = self.loss_fn(pred, y)
+
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            mean_loss.append(loss.item() / len(x))
+        mean_loss = np.sum(mean_loss) / len(mean_loss)
+        return mean_loss
+
+    def _validationloop(self, test_dataloader):
+        predict = []
+        true_y = []
+        with torch.no_grad():
+            for X, y in test_dataloader:
+                pred = self.network(X)
+                true_y = true_y + y.int().tolist()
+                predict = predict + pred.tolist()
+        fpr, tpr, thresholds = roc_curve(true_y, predict)
+        auc_value = auc(fpr, tpr)
+        return auc_value
+
+    def save(self, dir, name=None):
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        if name is not None:
+            dir = os.path.join(dir, name)
+        else:
+            dir = os.path.join(dir, "model")
+        torch.save(self.network, f"{dir}.pth")
+
+    def load(self, dir):
+        self.network.load_state_dict(torch.load(dir))
 
     def fit(
         self,
@@ -69,29 +93,24 @@ class BasicModel:
         train_dataloader = DataLoader(train_dataset, self.batch_size)
         del X_train, y_train, train_dataset
 
-        best_network = self.network
         best_auc = 0
         best_epoch = 0
         count = 0
         for i in range(1, self.max_epoch + 1):
-            self.network, self.loss_fn, self.optimizer, mean_loss = train_loop(
-                self.network, train_dataloader, self.loss_fn, self.optimizer
-            )
+            mean_loss = self._trainloop(train_dataloader)
             # 若果有验证集，则计算auc，保存最佳auc和最佳network，若5轮无提升，则循环终止， 返回auc
             if has_valid:
-                auc = validation_loop(
-                    self.network, valid_dataloader, self.loss_fn, self.optimizer
-                )
-                print((f"epoch {i}, loss: {mean_loss:.4f}| eval_auc: {auc:.4f}"))
-                if auc > best_auc:
-                    best_network = self.network
+                auc_value = self._validationloop(valid_dataloader)
+                print((f"epoch {i}, loss: {mean_loss:.4f}| eval_auc: {auc_value:.4f}"))
+                if auc_value > best_auc:
+                    self.save("./model/best_model")
                     best_auc = auc
                     best_epoch = i
                     count = 0
                 else:
                     count = count + 1
                     if count >= 5:
-                        self.network = best_network
+                        self.network = self.load("./model/best_model.pth")
                         print(
                             f"looping has early stopped, best epoch is {best_epoch}, which has auc {best_auc:.4f}"
                         )
@@ -113,4 +132,5 @@ class BasicModel:
         :param X_test: 测试集
         :return: 模型不同类的概率
         """
-        self.X_test = X_test
+        y_test = self.network(X_test)
+        return y_test
